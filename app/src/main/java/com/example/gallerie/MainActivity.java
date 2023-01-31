2f7549b133c65cca6377f2a743c7ec4fcb8d90dc
VarDumpMan <rabelaiskpechekou@gmail.com>
2023-01-31 18:23:59 +0200

    TP Import d'image dans la galerie Java

---
+++ app/src/main/java/com/example/gallerie/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.gallerie;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnImport;
+    private ImageView selectedImg;
+    static final int RESULT_LOAD_IMG = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnImport = (Button) findViewById(R.id.import_img);
+        selectedImg = (ImageView) findViewById(R.id.selected);
+
+        btnImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        System.out.println(resultCode);
+        System.out.println(reqCode);
+        System.out.println(data);
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                selectedImg.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Une erreur s'est produite",Toast.LENGTH_LONG).show();
+
+            }
+
+        }else {
+            Toast.makeText(getApplicationContext(),"Vous n'avez pas choisi d'image", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
